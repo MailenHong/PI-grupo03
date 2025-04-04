b679f64aa3b7efd7d6c921c19389750a1e05226d
ClaraSchmidt1 <mschmidt@udesa.edu.ar>
2025-04-04 13:18:40 -0300

    objetos literal en usuario.js

--- myapp/db/usuario.js
+++ myapp/db/usuario.js
@@ -2,14 +2,78 @@
 // todos los lugares donde aparezca el header logueado 
 // y en “mi perfil”.
 
-const usuario = {
-    email: 'mstoessel@gmail.com',
-    usuario: 'triple t',
-    contraseña: '',
-    nacimiento: "21/03/1997",
-    documento: 41123456,
-    perfil: '',
+const datos = {
+    usuario: {
+        email: 'mstoessel@gmail.com',
+        usuario: 'triple t',
+        contraseña: '',
+        nacimiento: "21/03/1997",
+        documento: 41123456,
+        perfil: '',
+    },
+    productos: [
+        {
+            id: 1,
+            imagen: '',
+            nombre: "Producto 1",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 2,
+            imagen: '',
+            nombre: "Producto 2",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 3,
+            imagen: '',
+            nombre: "Producto 3",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 4,
+            imagen: '',
+            nombre: "Producto 4",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 5,
+            imagen: '',
+            nombre: "Producto 5",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 6,
+            imagen: '',
+            nombre: "Producto 6",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 7,
+            imagen: '',
+            nombre: "Producto 7",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 8,
+            imagen: '',
+            nombre: "Producto 8",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 9,
+            imagen: '',
+            nombre: "Producto 9",
+            descripcion: "Descripción del producto"
+        },
+        {
+            id: 10,
+            imagen: '',
+            nombre: "Producto 10", 
+            descripcion: "Descripción del producto"}
+
+    ]
 }
 
-module.exports = usuario
+module.exports = datos
 
