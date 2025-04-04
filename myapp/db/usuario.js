1cd1d7a6a341804a6a70287577bfefe6621a5719
ClaraSchmidt1 <mschmidt@udesa.edu.ar>
2025-04-04 11:16:44 -0300

    cree carpeta db

---
+++ myapp/db/usuario.js
@@ -0,0 +1,15 @@
+// Los datos del usuario deben mostrarse en 
+// todos los lugares donde aparezca el header logueado 
+// y en “mi perfil”.
+
+const usuario = {
+    email: '',
+    usuario: '',
+    contraseña: '',
+    nacimiento: 0,
+    documento: 0,
+    perfil: '',
+}
+
+module.exports = usuario
+
