4b10dc29ea7a9dc126cec08a77c934b23f3efd07
MailenHong <hongm@udesa.edu.ar>
2025-04-10 02:11:43 -0300

    agregando userController con rutas

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -1,1 +1,11 @@
-const usuario = require('../db/datos')
+const datos = require('../db/datos')
+
+const userController = {
+  profile: function (req,res) {
+    res.render('profile', {
+        user: datos.usuario
+    });
+  }
+};
+
+module.exports = userController;
--- myapp/routes/product.js
+++ myapp/routes/product.js
@@ -1,4 +1,7 @@
 const express = require ('express');
 const router = express.Router();
+const productsController = require('../controllers/productController')
+
+router.get('/id', productsController.detalle);
 
 module.exports = router;
