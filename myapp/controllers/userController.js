5ab2ab87374eceeacecae1f9946eea8bfb38dbbd
ClaraSchmidt1 <mschmidt@udesa.edu.ar>
2025-04-11 12:55:43 -0300

    Conecté login/register y apliqué logo y marca RÜMA

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -1,6 +1,12 @@
 const datos = require('../db/datos')
 
 const userController = {
+  register: function (req,res) {
+    res.render('register');
+  },
+  login: function (req,res) {
+    res.render('login');
+  },
   profile: function (req,res) {
     res.render('profile', {
         user: datos.usuario
