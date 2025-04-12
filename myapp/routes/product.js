10b7f03d3894e75e164ea3cb4d6701d032b01c15
MailenHong <hongm@udesa.edu.ar>
2025-04-12 13:59:03 -0300

    realizando el product add

--- myapp/routes/product.js
+++ myapp/routes/product.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productsController = require('../controllers/productController')
 
 router.get('/id', productsController.detalle);
+router.get('/add', productsController.addform)
 
 module.exports = router;
