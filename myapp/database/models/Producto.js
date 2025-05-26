acaff8325d0cd94943c27a80cbe1f2d5d13f24e1
MailenHong <hongm@udesa.edu.ar>
2025-05-26 10:42:57 -0300

    creando modelo de producto

---
+++ myapp/database/models/Producto.js
@@ -0,0 +1,55 @@
+module.exports = function (sequelize, DataTypes) {
+    let alias = "Producto";
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER.UNSIGNED,
+
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+        },
+        imagen: {
+            type: DataTypes.STRING(255),
+        },
+        nombre: {
+            type: DataTypes.STRING(255),
+        },
+        descripcion: {
+            type: DataTypes.TEXT,
+        },
+        createdat: {
+            type: DataTypes.DATE,
+        },
+        updatedat: {
+            type: DataTypes.DATE,
+        },
+        deletedat: {
+            type: DataTypes.DATE,
+        }
+    };
+
+    let config = {
+        tableName: "productos",
+        timestamps: true,
+        underscored: true
+    };
+
+    let Producto = sequelize.define(alias, cols, config);
+
+    Producto.associate = function (models) {
+        Producto.belongsTo(models.Usuario, {
+            as: "usuario",
+            foreignKey: "FkUserId"
+        });
+
+         Producto.hasMany(models.Comentario,{
+            as:"comentarios",
+            foreignKey: "id"
+    });
+    };
+
+    return Producto;
+};
